3ee9c47ee3614b5a8d14c8332bfbfd0319aac896
Aurora2Link <aurora2link@gmail.com>
2025-02-28 01:03:51 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -53,4 +53,5 @@ def get_messages():
     return jsonify({"last_messages": messages})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host="0.0.0.0", port=int(os.getenv("PORT", 8080)))
+
