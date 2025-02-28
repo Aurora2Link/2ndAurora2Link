2b2820d9e3601431675639f534104bc668716592
Aurora2Link <aurora2link@gmail.com>
2025-02-28 03:18:11 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,14 @@ from flask import Flask, request, jsonify
 import redis
 import os
 from celery import Celery
+import json
+import logging
 
 app = Flask(__name__)
+
+# Configura el logger
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 # Variables globales
 Phone_number = "0000"
@@ -16,13 +22,22 @@ redis_client = redis.Redis.from_url(REDIS_URL, decode_responses=True)
 #Config Celery
 celery = Celery("tasks", broker=REDIS_URL, backend=REDIS_URL)
 
+def log_json(message, level="info"):
+    log_message = json.dumps(message)
+    if level == "info":
+        logger.info(log_message)
+    elif level == "warning":
+        logger.warning(log_message)
+    elif level == "error":
+        
+
 def check_redis():
     try:
         redis_client.ping()
-        print("Redis está conectado")
+        log_json({"message": "Redis está conectado"})
         return True
     except redis.exceptions.ConnectionError:
-        print("Redis no está disponible")
+        log_json({"message": "Redis no está disponible"}, level="error")
         return False
 
 
@@ -53,12 +68,12 @@ def process_message(data):
     try:
         Phone_number = data['entry'][0]['changes'][0]['value']['messages'][0]['from']
         Message = data['entry'][0]['changes'][0]['value']['messages'][0]['text']['body']
-        print(f"Message recived from {Phone_number}: {Message}")
+        log_json({"phone_number": Phone_number, "message": Message})
         redis_client.lpush("message_queue", f"{Phone_number}:{Message}")
-        print("Message stored in Redis successfully.")
+        log_json({"message": "Message stored in Redis successfully."})
 
     except Exception as e:
-        print(f"Error while processing the message: {str(e)}")
+        log_json({"error": f"Error while processing the message: {str(e)}"}, level="error")
 
 @app.route('/messages')
 def get_messages():
